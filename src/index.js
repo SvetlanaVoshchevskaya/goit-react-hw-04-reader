2adff00fa89ab5104034f4af50fa1b7e68eb5964
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-07-18 19:15:17 +0300

    change

--- src/index.js
+++ src/index.js
@@ -5,7 +5,7 @@ import './index.css';
 import App from './App';
 
 ReactDOM.render(
-  <HashRouter>
+  <HashRouter basename="/">
     <Route component={App} />
   </HashRouter>,
   document.getElementById('root'),
