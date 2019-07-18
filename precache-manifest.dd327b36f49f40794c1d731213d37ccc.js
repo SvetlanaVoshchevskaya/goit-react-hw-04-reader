56ae9724bc953bdeede0a59ae18f0b6d67ca6ab2
Svetlana Voshchevskaya <VSveta.83@gmail.com>
2019-07-18 18:48:21 +0300

    Updates

--- precache-manifest.dd327b36f49f40794c1d731213d37ccc.js
+++ precache-manifest.dd327b36f49f40794c1d731213d37ccc.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "70c9f0b026cef595167c68b8d8a8e412",
+    "revision": "1dee7328344618fab4bf9e4324658d43",
     "url": "/goit-react-hw-04-reader/index.html"
   },
   {
-    "revision": "cb4c5be69f6e67667eb1",
+    "revision": "3777a7904d4d05d744e1",
     "url": "/goit-react-hw-04-reader/static/css/main.3212e3dd.chunk.css"
   },
   {
@@ -12,8 +12,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/goit-react-hw-04-reader/static/js/2.2bf15cd3.chunk.js"
   },
   {
-    "revision": "cb4c5be69f6e67667eb1",
-    "url": "/goit-react-hw-04-reader/static/js/main.0b8a4daf.chunk.js"
+    "revision": "3777a7904d4d05d744e1",
+    "url": "/goit-react-hw-04-reader/static/js/main.40060725.chunk.js"
   },
   {
     "revision": "84bf89e08b01550b9b7d",
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/goit-react-hw-04-reader/precache-manifest.b364fbefc9f054363fce276c3ee7814c.js"
+  "/goit-react-hw-04-reader/precache-manifest.dd327b36f49f40794c1d731213d37ccc.js"
 );
 
 self.addEventListener('message', (event) => {
